8877ae13b3b0d4131fe46d20589846122fe8d715
Tim Horist <TimelikeClosure@users.noreply.github.com>
2016-12-19 17:36:19 -0800

    discovered given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -40,8 +40,13 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      productsICanEat = _.filter(products, function (product) {
+        return !product.containsNuts && _.all(product.ingredients, function (ingredient) {
+          return ingredient !== "mushrooms";
+        });
+      });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
